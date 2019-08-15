a6a11d9dd2752407ffd31e260d49a13e8a138c97
harsh jain <harshjniitr@gmail.com>
2019-08-15 20:08:02 +0530

    Add download

--- dashboard/src/containers/NavBar.js
+++ dashboard/src/containers/NavBar.js
@@ -9,6 +9,7 @@ import MenuIcon from '@material-ui/icons/Menu';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
+import CloudDownloadIcon from '@material-ui/icons/CloudDownload';
 import './NavBar.css'
 import { connect } from 'react-redux';
 
@@ -16,93 +17,99 @@ import { connect } from 'react-redux';
 
 
 class NavBar extends Component {
-    constructor(){
-        super()
-        this.handleMenu = this.handleMenu.bind(this)
-        this.handleClose = this.handleClose.bind(this)
-        this.signOut = this.signOut.bind(this)
-        this.state = {anchor: null, open: false, user: {}}
-    }
+  constructor() {
+    super()
+    this.handleMenu = this.handleMenu.bind(this)
+    this.handleClose = this.handleClose.bind(this)
+    this.signOut = this.signOut.bind(this)
+    this.state = { anchor: null, open: false, user: {} }
+  }
 
-    handleMenu(event) {
-        this.setState({anchor: event.currentTarget, open: !this.state.open});
+  handleMenu(event) {
+    this.setState({ anchor: event.currentTarget, open: !this.state.open });
+  }
+  handleClose(event) {
+    this.setState({ anchor: null, open: false });
+  }
+  signOut(e) {
+    e.preventDefault()
+    this.props.userSession.signUserOut()
+    window.location = '/'
+  }
+  componentWillMount() {
+    if (!this.state.user.hasOwnProperty('username')) {
+      this.setState({ user: this.props.userSession.loadUserData() })
     }
-    handleClose(event) {
-        this.setState({anchor: null, open: false});
-    }
-    signOut(e) {
-        e.preventDefault()
-        this.props.userSession.signUserOut()
-        window.location = '/'
-    }
-    componentWillMount(){
-        if (!this.state.user.hasOwnProperty('username')){
-            this.setState({ user: this.props.userSession.loadUserData()})
-        }
-    }
-  render(){
+  }
+  render() {
 
-  return (
-    <div >
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start"  color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className="userImage" >
-            {this.props.title}
-          </Typography>
-          <Typography variant="h6" >
-            {this.state.user.username}
-          </Typography>
-          {true && (
-            <div>
-              <IconButton
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={this.handleMenu}
-                color="inherit"
-              >
-                <AccountCircle />
+    return (
+      <div >
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton edge="start" color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className="userImage" >
+              {this.props.title}
+            </Typography>
+            <Typography variant="h6" className="username">
+              {this.state.user.username}
+            </Typography>
+            <a target='_blank' href="https://github.com/harsh-98/tunzal/releases" style={{color: 'white'}}>
+              <IconButton edge="start" color="inherit" aria-label="menu">
+              <CloudDownloadIcon/>
               </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={this.state.anchor}
-                anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                open={this.state.open}
-                onClose={this.handleClose}
-              >
-                <MenuItem onClick={()=> {window.location=this.props.endpoint}}>{this.props.message}</MenuItem>
-                <MenuItem onClick={this.signOut}>Sign out</MenuItem>
-              </Menu>
-            </div>
-          )}
-        </Toolbar>
-      </AppBar>
-    </div>
-  )}
+              </a>
+            {true && (
+              <div>
+                <IconButton
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={this.handleMenu}
+                  color="inherit"
+                >
+                  <AccountCircle />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={this.state.anchor}
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  open={this.state.open}
+                  onClose={this.handleClose}
+                >
+                  <MenuItem onClick={() => { window.location = this.props.endpoint }}>{this.props.message}</MenuItem>
+                  <MenuItem onClick={this.signOut}>Sign out</MenuItem>
+                </Menu>
+              </div>
+            )}
+          </Toolbar>
+        </AppBar>
+      </div>
+    )
+  }
 }
 
 const mapStateToProps = state => {
-    return {
-        userSession: state.userSession,
-    }
+  return {
+    userSession: state.userSession,
+  }
 };
 
 const mapDispatchToProps = dispatch => {
-    return {
-    }
+  return {
+  }
 };
 
 export default connect(
-    mapStateToProps,
-    mapDispatchToProps)(NavBar);
+  mapStateToProps,
+  mapDispatchToProps)(NavBar);
